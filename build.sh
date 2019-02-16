5fc31e6a4a0f6b912f0b5157a5635497b4acb71f
Laurent Senta <laurent.senta@gmail.com>
2019-02-16 12:05:53 +0100

    autobuild and image size in references

--- build.sh
+++ build.sh
@@ -14,34 +14,49 @@ __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on
 
 args="${1:-}"
 
-# Script
-SRC=${__dir}/src
-PROJECT=${args[0]}
-PROJECT_NAME=hugoblocks-${PROJECT}
-PROJECT_SRC=${SRC}/${PROJECT_NAME}
-HUGOBLOCKS_SRC=${__dir}/src/hugoblocks
-PROJECT_BUILD="${__dir}/build/${PROJECT_NAME}"
-SASS="${__dir}/node_modules/.bin/node-sass-chokidar"
-
 function usage() {
   >&2 echo "./build.sh [PROJECT]"
   >&2 echo "    Project could be: explo, flow, focus, zeno, etc."
 }
 
-if [[ -z "${PROJECT}" ]]; then
+function build() {
+  SRC=${__dir}/src
+  PROJECT=$1
+  PROJECT_NAME=hugoblocks-${PROJECT}
+  PROJECT_SRC=${SRC}/${PROJECT_NAME}
+  HUGOBLOCKS_SRC=${__dir}/src/hugoblocks
+  PROJECT_BUILD="${__dir}/build/${PROJECT_NAME}"
+  SASS="${__dir}/node_modules/.bin/node-sass-chokidar"
+
+  if [[ ! -d "${PROJECT_SRC}" ]]; then
+    >&2 echo "project source not found in:" ${PROJECT_SRC}; usage; exit 1;
+  fi
+
+  echo "building ${PROJECT}"
+
+  mkdir -p ${PROJECT_BUILD}
+  mkdir -p ${PROJECT_BUILD}/static/css/
+
+  cp -R ${HUGOBLOCKS_SRC}/*  ${PROJECT_BUILD}/
+  cp -R ${PROJECT_SRC}/* ${PROJECT_BUILD}/
+  ${SASS} ${PROJECT_SRC}/style/style.scss     \
+    --source-map true                         \
+    ---include-path ${SRC} \
+    -o ${PROJECT_BUILD}/static/css/
+}
+
+if [[ -z "${args[0]}" ]]; then
   >&2 echo "no project given."; usage; exit 1;
 fi
 
-if [[ ! -d "${PROJECT_SRC}" ]]; then
-  >&2 echo "project source not found in:" ${PROJECT_SRC}; usage; exit 1;
+ALL_PROJECTS=(explo flow focus zeno)
+
+if [ "${args[0]}" == "all" ]; then
+  for project in ${ALL_PROJECTS[*]}; do
+    build "${project}";
+  done
+else
+  build "${args[0]}"
 fi
 
-mkdir -p ${PROJECT_BUILD}
-mkdir -p ${PROJECT_BUILD}/static/css/
-
-cp -R ${HUGOBLOCKS_SRC}/*  ${PROJECT_BUILD}/
-cp -R ${PROJECT_SRC}/* ${PROJECT_BUILD}/
-${SASS} ${PROJECT_SRC}/style/style.scss     \
-  --source-map true                         \
-  ---include-path ${SRC} \
-  -o ${PROJECT_BUILD}/static/css/
+echo "done"
